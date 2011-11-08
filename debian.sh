91367908e2881f78ff64c201e8f5c389c5ba9dd9
eyealike <hannes@eyealike.com>
2011-11-07 19:00:44 -0800

    Tweaked versioning such that libcassandra follows Cassandra

--- debian.sh
+++ debian.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -ex
 
-psPlatformVersion="3.3.0"
+psPlatformVersion="ps3.4.0"
 distribution="lucid"
-upstreamVersion="0.1"
-debianVersion="0ubuntu${psPlatformVersion}"
+upstreamVersion="1.0.0"
+debianVersion="0ubuntu1"
 
 config/autorun.sh
 
 dch \
 	--distribution "${distribution}" \
 	--force-distribution \
-	--newversion "${upstreamVersion}-${debianVersion}~${BUILD_NUMBER}" \
+	--newversion "${upstreamVersion}-${debianVersion}-${psPlatformVersion}~${BUILD_NUMBER}" \
 	"Continuous integration build #${BUILD_NUMBER}"
 
 debuild -i -us -uc -b
